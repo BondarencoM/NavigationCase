52f6dce6624b2e6d5625b1b7d2a4949bdce3db57
BondarencoM <bondarenco.mihail@gmail.com>
2022-04-01 14:52:21 +0200

    Make it mostly work

--- green_turtle/green_turtle/controller.py
+++ green_turtle/green_turtle/controller.py
@@ -1,27 +1,27 @@
 from threading import Thread
 import rclpy
-from rclpy.impl import rcutils_logger
+from rclpy.qos import qos_profile_sensor_data
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from green_interfaces.srv import Scan
 from nav2_msgs.action import NavigateToPose
 from geometry_msgs.msg import PoseStamped
-from std_msgs.msg import String
+from std_msgs.msg import String, Header
 
 import time
 
 coords = [
-	("0", (-6.26329, -4.64111, 0.0), (0.0, 0.0, 0.00848949, 0.999964), 0.0169792),
-	("1", (-0.841427, 2.67423, 0.0), (0.0, 0.0, 0.0137728, 0.999905), 0.0275465),
-	("2", (2.30796, 0.747669, 0.0),  (0.0, 0.0, 0.0122631, 0.999925), 0.0245268),
-	("3", (1.05082, -4.55658, 0.0),  (0.0, 0.0, 0.000976583, 1), 0.00195317),
-	("4", (3.16242, 4.8034, 0.0),    (0.0, 0.0, 1, 0.000397207), 3.1408),
-	("5", (0.878812, -3.00759, 0.0), (0.0, 0.0, 0.701042, 0.71312), 1.55372),
-	("6", (-5.962, 4.91293, 0.0),    (0.0, 0.0, -0.999708, 0.0241475), -3.09329),
-	("7", (4.36006, -7.47229, 0.0),  (0.0, 0.0, -0.696966, 0.717104), -1.54232),
-	("8", (8.05787, -8.03957, 0.0),  (0.0, 0.0, -0.0153137, 0.999883), -0.0306286),
-	("9", (4.3872, 7.97167, 0.0),    (0.0, 0.0, 0.642645, 0.766164), 1.39589),
-	("10", (9.80156, -2.75549, 0.0),  (0.0, 0.0, -0.00746938, 0.999972),-0.0149389),
+	("0", (-6.2, -4.6, 0.0),(0.008, 0.999)),
+	("1", (-0.8, 2.6, 0.0), (0.0137, 0.999)),
+	("2", (2.3, 0.7, 0.0),  (0.0122, 0.999)),
+	("3", (1.0, -4.5, 0.0), (0.0, 1.0)),
+	("4", (3.1, 4.8, 0.0),  (1, 0.000)),
+	("5", (0.8, -3.0, 0.0), (0.701, 0.713)),
+	("6", (-5.9, 4.9, 0.0), (-0.999, 0.0241)),
+	("7", (4.3, -7.4, 0.0), (-0.696, 0.717)),
+	("8", (8.0 -8.0, 0.0),  (-0.0153, 0.999)),
+	("9", (4.3, 7.9, 0.0),  (0.642, 0.7661)),
+	("10", (9.8, -2.7, 0.0),(-0.007, 0.999)),
 ]
 
 class Controller(Node):
@@ -30,30 +30,35 @@ class Controller(Node):
 
 	def __init__(self):
 		super().__init__('green_turtle_controller')
-		self.navigation = self.create_publisher(PoseStamped, '/goal_pose', 10)
+
+		self.navigation = self.create_publisher(PoseStamped, '/goal_pose', qos_profile_sensor_data)
 		self.scaner = self.create_subscription(String, '/scans', self.scan_received, 10)
-		# self._actionclient = ActionClient(self, NavigateToPose, '/navigate_to_pose')
-		# self.srv = self.create_service(Scan, '/start', self.send_goal)
+		self._actionclient = ActionClient(self, NavigateToPose, '/navigate_to_pose', )
+		# # self.srv = self.create_service(Scan, '/start', self.send_goal)
 
 	def scan_received(self, msg):
 		self.get_logger().info(f"QR data= {msg.data}")
 		self.last_qr = msg.data
 
 	def start_navigation(self, coord):
-		_, pos, orientation, _ = coord
+		_, pos, orientation = coord
 
 
 		pose = PoseStamped()
-		pose.pose.position.x, pose.pose.position.y, pose.pose.position.z = pos
-		pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w = orientation
-		pose.header.frame_id = 'map'
-
-		self.get_logger().info(f"Sending navigation goar for pos {pose}")
+		pose.pose.position.x, pose.pose.position.y, _ = pos
+		pose.pose.orientation.z, pose.pose.orientation.w = (0.0, 1.0)
+		pose.header = Header()
+		pose.header.frame_id = "odom"
+		self.get_logger().info(f"Sending navigation goal for pos {pose}")
 
 		# self.navigation.publish(pose)
 		goal = NavigateToPose.Goal()
 		goal.pose = pose
 		
+		self.navigation.publish(pose)
+		time.sleep(3)
+		self.navigation.publish(pose)
+		time.sleep(3)
 		self.navigation.publish(pose)
 
 	def check_qr(self, expected):
@@ -66,10 +71,12 @@ def main(args=None):
 
 	for coord in coords:
 		service.start_navigation(coord)
-
+		
 		while not service.check_qr(coord[0]):
 			print("waiting")
+			time.sleep(1)
 			rclpy.spin_once(service, timeout_sec=1)
+
 		time.sleep(10)
 
 
